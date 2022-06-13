531da7ff9a47cc31dcf2399646354a1c4589071c
Tudor Goia <mindsetcoach360@gmail.com>
2022-06-13 22:51:01 +0300

    our first API call

---
+++ main.js
@@ -0,0 +1,12 @@
+
+axios
+    .get('https://api.spacexdata.com/v4/crew')
+    .then(function(response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function(error) {
+        // handle error
+        console.log(error);
+    })
+;
