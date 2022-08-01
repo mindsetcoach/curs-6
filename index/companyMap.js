91a3ded3c92c15ba880a688d1d093bbc46aa5f20
Tudor Goia <mindsetcoach360@gmail.com>
2022-08-01 21:09:11 +0300

    added a map with the landpads and the launchpads on it

---
+++ index/companyMap.js
@@ -0,0 +1,60 @@
+index.companyMap = {
+
+    map: null,
+
+    initialize: function() {
+
+        if (index.companyMap.map === null) {
+            index.companyMap.map = L
+                .map('map')
+                .setView(
+                    [40, 0],
+                    2
+                )
+            ;
+
+            L
+                .tileLayer(
+                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    {
+                        maxZoom: 19,
+                        attribution: '© OpenStreetMap'
+                    }
+                )
+                .addTo(index.companyMap.map)
+            ;
+        }
+    },
+
+    render: function(type) {
+
+        if (type === 'launchpads') {
+            let launchpadsData = index.data.launchpadsData;
+            for (let i = 0; i < launchpadsData.length; i++) {
+                L
+                    .marker([launchpadsData[i].latitude, launchpadsData[i].longitude])
+                    .addTo(index.companyMap.map)
+                ;
+            }
+        }
+        if (type === 'landpads') {
+            let landpadsData = index.data.landpadsData;
+            for (let i = 0; i < landpadsData.length; i++) {
+                L
+                    .circle(
+                        [landpadsData[i].latitude, landpadsData[i].longitude],
+                        {
+                            color: 'red',
+                            fillColor: '#f03',
+                            fillOpacity: 0,
+                            radius: 200000
+                        }
+                    )
+                    .addTo(index.companyMap.map)
+                ;
+            }
+        }
+
+    }
+
+};
--- index/main.js
+++ index/main.js
@@ -1,14 +1,32 @@
 let index = {
 
     data: {
-        companyInfo: null
+        companyInfo: null,
+        launchpadsData: null,
+        landpadsData: null
     },
 
     initialize: function() {
 
         axios
             .get('https://api.spacexdata.com/v4/company')
-            .then(index.onAxiosFinished)
+            .then(index.onCompanyInfoAvailable)
+            .catch(function(error) {
+                // handle error
+                console.log(error);
+            })
+        ;
+        axios
+            .get('https://api.spacexdata.com/v4/launchpads')
+            .then(index.onLaunchpadsDataAvailable)
+            .catch(function(error) {
+                // handle error
+                console.log(error);
+            })
+        ;
+        axios
+            .get('https://api.spacexdata.com/v4/landpads')
+            .then(index.onLandpadsDataAvailable)
             .catch(function(error) {
                 // handle error
                 console.log(error);
@@ -17,12 +35,26 @@ let index = {
 
     },
 
-    onAxiosFinished: function(response) {
+    onCompanyInfoAvailable: function(response) {
 
         index.data.companyInfo = response.data;
 
         index.companyInfo.render();
     },
+    onLaunchpadsDataAvailable: function(response) {
+
+        index.data.launchpadsData = response.data;
+
+        index.companyMap.initialize();
+        index.companyMap.render('launchpads');
+    },
+    onLandpadsDataAvailable: function(response) {
+
+        index.data.landpadsData = response.data;
+
+        index.companyMap.initialize();
+        index.companyMap.render('landpads');
+    }
 };
 
 index.initialize();
