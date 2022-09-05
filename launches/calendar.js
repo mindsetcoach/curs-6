b6fd4eb9cd542c9c126e3ca324d1635c1ada41a4
Tudor Goia <mindsetcoach360@gmail.com>
2022-09-05 21:03:48 +0300

    some more steps towards finishing the calendar

--- launches/calendar.js
+++ launches/calendar.js
@@ -1,9 +1,21 @@
 launches.calendar = {
 
+    monthsOfYear: [
+        'January', 'February', 'March', 'April',
+        'May', 'June', 'July', 'August',
+        'September', 'October', 'November', 'December',
+    ],
+
     init: function() {
 
         launches.data.calendarPage.year = moment().year();
         launches.data.calendarPage.month = moment().month() + 1;
+
+        let prevButton = document.querySelector('.si-calendarModal .si-prevButton');
+        let nextButton = document.querySelector('.si-calendarModal .si-nextButton');
+
+        prevButton.addEventListener('click', launches.calendar.onPrevClicked);
+        nextButton.addEventListener('click', launches.calendar.onNextClicked);
 
         launches.calendar.render();
     },
@@ -18,14 +30,36 @@ launches.calendar = {
         let daysInSelectedMonth = selectedAsMoment.daysInMonth();
         let firstMonthDayAsWeekDayNumber = selectedAsMoment.day();
 
+        let parentOfSquares = document.querySelector('.si-calendarModal .si-calendarCore');
+        parentOfSquares.innerHTML = '';
+        let calendarPageSpan = document.querySelector('.si-calendarModal .si-calendarPage');
+        calendarPageSpan.innerHTML = launches.calendar.monthsOfYear[selectedMonth - 1];
+
         for (let i = 0; i < firstMonthDayAsWeekDayNumber; i++) {
             // put an empty square in HTML
+            let emptySquare = document.createElement('div');
+            emptySquare.classList.add('si-day');
+            parentOfSquares.appendChild(emptySquare);
         }
         for (let i = 0; i < daysInSelectedMonth; i++) {
             // put a day square in HTML
+            let square = document.createElement('div');
+            square.classList.add('si-day');
+            square.classList.add('si-borderedDay');
+            square.innerHTML = i + 1;
+            parentOfSquares.appendChild(square);
         }
 
-        alert(firstMonthDayAsWeekDayNumber)
+    },
+
+    onPrevClicked: function() {
+        launches.data.calendarPage.month -= 1;
+        launches.calendar.render();
+    },
+
+    onNextClicked: function() {
+        launches.data.calendarPage.month += 1;
+        launches.calendar.render();
     }
 
 };
